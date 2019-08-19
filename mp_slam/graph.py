376958d7d72ea40a156bc08ddaf2241c1b505945
Jariullah Safi <safijari@isu.edu>
2019-08-18 23:25:12 -0700

    Adding graph implementation

---
+++ mp_slam/graph.py
@@ -0,0 +1,98 @@
+"""
+Ok let's make a 2D pose graph in python
+
+What I need:
+- A node class that contains
+"""
+
+
+class LinkLabel(object):
+    def __init__(self, pose, mean, covariance):
+        self.pose = pose
+        self.mean = mean
+        self.covariance = covariance
+
+
+class Vertex(object):
+    def __init__(self, obj):
+        self.obj = obj
+        self.edges = []
+
+    def add_edge(self, edge):
+        self.edges.append(edge)
+
+    def get_adjacent_vertices(self):
+        out = []
+        for edge in self.edges:
+            if edge.source != self:
+                out.append(edge.source)
+            if edge.target != self:
+                out.append(edge.target)
+        return out
+
+
+class Edge(object):
+    def __init__(self, source, target, link_info):    # Vertices
+        self.source = source
+        self.target = target
+        self.info = link_info    # (interface: pose, mean, covar)
+        self.source.add_edge(self)
+        self.target.add_edge(self)
+
+
+class Graph(object):
+    def __init__(self):
+        self.vertices = {}
+        self.edges = []
+
+    def add_vertex(self, name, vertex):
+        self.vertices[name] = vertex
+        # this is where a node is added to SBA?
+
+    def add_edge(self, edge):
+        """
+        Karto does this in MapperGraph over the current vertex (obj?) as such:
+        - Link to previous obj
+        - Link to the "running scans array" (link to chain) (karto keeps track of the means of this obj as well as an array of the same covariance for this obj(???))
+          - This has a distance threshold and only links to one obj from the "running scans array" which is the closest (so in general it should only link to the previous obj)
+          - I don't know how necessary this is honestly
+        - Link to "near chains" (that do not contain this new obj)
+          - Get "near chains"
+        """
+        self.edges.append(edge)
+        # this is where a constrant is added to SBA?
+
+
+def do_breadth_first_traversal(start_vert, visit_fn, return_objs=True):
+    """
+    visit_fn takes in first node and current node and returns
+    true if the traversal should continue
+    """
+    to_visit = []
+    seen_verts = set()
+    valid_verts = []
+
+    to_visit.push(start_vert)
+    seen_verts.add(start_vert)
+
+    while to_visit:
+        vert = to_visit.pop()
+        if (not visit_fn(start_vert, vert)):
+            continue
+
+        valid_verts.append(vert)
+
+        for avert in vert.get_adjacent_vertices():
+            if avert in seen_verts:
+                continue
+            to_visit.append(avert)
+            seen_verts.add(avert)
+
+    if return_objs:
+        return [v.obj for v in valid_verts]
+    return valid_verts
+
+"""
+def link_scans(scan1, scan2, mean_scan1, covar_scan1):
+    (don't link if link already exists)
+"""
