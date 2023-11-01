373cb8c513a716e5d136e804e3f156666b098c6f
Jariullah Safi <safijari@isu.edu>
2023-11-01 13:04:19 -0500

    facepalm

--- run_docker.sh
+++ run_docker.sh
@@ -6,3 +6,10 @@ cd /catkin_ws/src/yag-slam/ros1/
 
     # container_jailbreak_if_needed("docker run -it --name=yag-slam --rm --net host gcr.io/jari-cloud/yag-slam /catkin_ws/src/yag-slam/run_docker.sh")
     # container_jailbreak_if_needed("docker container kill yag-slam")
+
+    # initialPose = get_last_pose_in_map_frame()
+    # xx, yy, tt = [initialPose["x"], initialPose["y"], initialPose["yaw_rad"]]
+    # map_path = get_current_map_path()
+    # container_jailbreak_if_needed('docker run -it -v $HOME:$HOME --name=yag-slam --rm --net host gcr.io/jari-cloud/yag-slam /catkin_ws/src/yag-slam/run_docker.sh -b "{}" -x {} -y {} -t {}'.format(map_path, xx, yy, tt))
+    # container_jailbreak_if_needed("docker container kill yag-slam")
+
